d899a5615401c12349a96a219601c4e14260b7e7
Rafael Enes <rafaelenesclausen@gmail.com>
2025-08-14 17:12:19 -0300

    first commit

---
+++ src/main/java/com/fullcycle/admin/fc3_catalogo/Fc3CatalogoApplication.java
@@ -0,0 +1,13 @@
+package com.fullcycle.admin.fc3_catalogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fc3CatalogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fc3CatalogoApplication.class, args);
+	}
+
+}
